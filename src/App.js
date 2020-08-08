cb7af39e59104b0a245531823c1807cc2407dbf4
Oscar Peulicke <oscarrpeulicke@gmail.com>
2020-08-08 13:00:22 +0200

    to get started

--- src/App.js
+++ src/App.js
@@ -432,10 +432,10 @@ export default props => {
             </Grid>
             <b>To get started:</b>
             <ul>
-                <li>Press "Initialize"</li>
-                <li>Switch on automatic growth and set the volume fraction to at least 30</li>
+                <li>Under "Setup" Press "Initialize"</li>
+                <li>Under "After setup" turn on automatic growth and set the volume fraction to at least 30</li>
                 <li>Wait for the axons to grow</li>
-                <li>Switch the view mode of axons to "pipes" to see the final result</li>
+                <li>Under "Visual" set the axons to be displayed as "pipes" to see the final result</li>
             </ul>
             <br />
             <InputFormat />
