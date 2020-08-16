bc1625eb78ed60ca6beab14591a8a9fcf88f03e2
Oscar Peulicke <oscarrpeulicke@gmail.com>
2020-08-16 13:55:34 +0200

    documentation visual

--- src/Help/Visual.js
+++ src/Help/Visual.js
@@ -1,3 +1,51 @@
 import React from "react";
 
-export default props => <>TODO</>;
+export default props => (
+    <>
+        In the "Visual" pane you can specify how the generated substrate should be displayed:
+        <ul>
+            <li>
+                <b>Voxels</b>
+                <ul>
+                    <li>
+                        <b>Hide</b> - Hide the voxels.
+                    </li>
+                    <li>
+                        <b>Show</b> - Show the voxels.
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <b>Axons</b>
+                <ul>
+                    <li>
+                        <b>Hide</b> - Hide the axons.
+                    </li>
+                    <li>
+                        <b>Ellipsoids</b> - Display the aoxns as chains of ellipsoids (this is what is used by the
+                        algorithm).
+                    </li>
+                    <li>
+                        <b>Skeleton</b> - Display the axons as skeletons (a line strip that represents the center of the
+                        axon).
+                    </li>
+                    <li>
+                        <b>Pipes</b> - Display the axons as hollow "pipes" (should be used when exporting).
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <b>Cells</b>
+                <ul>
+                    <li>
+                        <b>Hide</b> - Hide the cells.
+                    </li>
+                    <li>
+                        <b>Show</b> - Show the cells.
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        Clicking the "Export" button will export the currently displayed scene in PLY format.
+    </>
+);
