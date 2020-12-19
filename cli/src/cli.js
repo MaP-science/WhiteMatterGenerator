7994191e11ca70f2d94e1847139f5f09a5f17f15
Oscar <oscarrpeulicke@gmail.com>
2020-12-19 16:20:28 +0100

    cli yargs

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -1,18 +1,28 @@
 #!/usr/bin/env node
 
-import commandLineArgs from "command-line-args";
+var { argv } = require("yargs")
+    .scriptName("axon-generator-toolbox")
+    .usage("Usage: $0 -f file [-i iterations]")
+    .example("$0 -f config.json -i 10")
+    .option("f", {
+        alias: "file",
+        describe: "Config file",
+        demandOption: "Please specify a config file",
+        type: "string",
+        nargs: 1
+    })
+    .option("i", {
+        alias: "iterations",
+        default: 10,
+        describe: "Number of iterations",
+        type: "number",
+        nargs: 1
+    });
+
 import fs from "fs";
 import { Synthesizer } from "@axon-generator-toolbox/core/dist/index.js";
 
-const optionDefinitions = [
-    { name: "src", type: String, defaultOption: true },
-    { name: "iterations", alias: "i", type: Number }
-];
-
-const options = commandLineArgs(optionDefinitions);
-
-const data = JSON.parse(fs.readFileSync(options.src));
-const iterations = options.iterations || 10;
+const data = JSON.parse(fs.readFileSync(argv.file));
 
 const growSpeed = data.growSpeed;
 const contractSpeed = data.contractSpeed;
@@ -20,11 +30,11 @@ const border = data.border;
 
 const synthesizer = new Synthesizer(data);
 
-const outputDir = "output";
+const outputDir = argv.file.substring(0, argv.file.lastIndexOf("/")) + "/output";
 
 if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
 
-for (let i = 0; i < iterations; ++i) {
+for (let i = 0; i < argv.iterations; ++i) {
     do synthesizer.update(growSpeed, contractSpeed, 0.01, 0.0001, 0);
     while ((synthesizer.updateState || {}).name !== "ready");
     const config = { growSpeed: growSpeed, contractSpeed: contractSpeed, border: border, ...synthesizer.toJSON() };
